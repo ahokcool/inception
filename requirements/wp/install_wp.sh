328c47a53597dc27b0cf22eb6bf2339980f18296
astein <stein.alexander@posteo.de>
2024-05-23 20:51:59 +0100

    enough for today

---
+++ requirements/wp/install_wp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ensure the script ends if any command fails
+set -e
+
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+# Start php-fpm in the foreground
+php-fpm7.4 -F
