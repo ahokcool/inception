2fbbb0d3a994b37f9dc524def0cccdd1b0ee995b
astein <stein.alexander@posteo.de>
2024-05-29 16:10:19 +0100

    still not working

--- src/requirements/db/setup_db.sh
+++ src/requirements/db/setup_db.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 
-# The script should stop on the first error
-set -e
-
-# Change the bind adress
-sed -i 's/^bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
-
-echo "Installing mariadb database..."
-mariadb-install-db
-#--skip-test-db --basedir=/usr --datadir=/var/lib/mysql
-echo "Starting MariaDB..."
-service mariadb start
-service mariadb status
-sleep 10
-##
-##echo "Change root password..."
-##mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
-##
-#echo "Create new user '$MYSQL_USER' and grant privileges..."
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-#mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-#mysql -u root -e "FLUSH PRIVILEGES;"
-##
-##echo "Create database..."
-##mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-##
-##echo "Creating database..."
-##service mariadb status
-##
-#echo "Stopping MariaDB..."
-sleep 5
-service mariadb stop
-sleep 5
+#if [ ! -d "/var/lib/mysql/wordpress" ]; then
 #
-echo "Starting MariaDB in foreground..."
-exec mysqld_safe --skip-grant-tables
+#echo "Initializing MariaDB database..."
+#
+#cat << EOF > /tmp/create_db.sql
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '123qwe321qwe';
+#FLUSH PRIVILEGES;
+#EOF
+#
+#    # run init.sql
+#    mysqld --bootstrap < /tmp/create_db.sql
+#    rm -f /tmp/create_db.sql
+#fi
